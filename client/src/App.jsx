dd5dd2efc589d557cdee51b7983492c37fa06798
saritasubhra <saritasubhra1@gmail.com>
2025-01-08 12:55:16 +0530

    signup and login page

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="p-4 h-screen flex items-center justify-center bg-slate-300">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
